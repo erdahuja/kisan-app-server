7a2781584fb03b60dc26ca30e008ee4c9352b88b
Deepak <er.d.ahuja@gmail.com>
2018-07-05 19:28:39 +0530

    fix: update history push

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  console.log(req.body);
   next();
 });
 app.use('/', index);
